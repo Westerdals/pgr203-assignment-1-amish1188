809d1845ef8cb349bb33fe3239d50cc0618abda4
amish1188 <martin.joodd@gmail.com>
2019-09-07 11:20:08 +0200

    first test

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 [![Build Status](https://travis-ci.com/amish1188/pgr203-excercise.svg?branch=master)](https://travis-ci.com/amish1188/pgr203-excercise)
+
+
+PGR Excercises
+
+*Yatzi
+    *Ones
+    *Twos
+    *Threes
+    *Fours
+    *Fives
+    *Sixes
+    *Pair
+    *Two pair
+    *Full house
+    *Small straight
+    *Large straight
--- src/test/java/no/kristiania/yatzy/yatzyGame.java
+++ src/test/java/no/kristiania/yatzy/yatzyGame.java
@@ -7,14 +7,22 @@ public class yatzyGame {
 
     @Test
     void getScore(){
-        assertEquals(0, scoreYatzy(YatziCategory.ONES, new int[] {1,2,3,4,5,6}));
+        assertEquals(1, scoreYatzy(YatziCategory.ONES, new int[] {1,2,3,4,5,6}));
     }
 
 
 
     public int scoreYatzy(YatziCategory cat, int[] dice) {
+        int [] frequencies = new int[7];
+        for(int die : dice){
+            frequencies[die]++;
+        }
+        for(int i=0;i<frequencies.length;i++){
+            return frequencies[1];
+        }
         return 0;
     }
+
 
 
 

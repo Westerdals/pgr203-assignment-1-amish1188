f7db7b25130da38b9c6d5dfdedae2a58dd48e42d
amish1188 <martin.joodd@gmail.com>
2019-09-07 10:57:09 +0200

    travis added

--- src/test/java/no/kristiania/yatzy/yatzyGame.java
+++ src/test/java/no/kristiania/yatzy/yatzyGame.java
@@ -1,10 +1,19 @@
 package no.kristiania.yatzy;
+        import org.junit.jupiter.api.Test;
+        import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class yatzyGame {
 
     @Test
     void getScore(){
-        assertEquals()
+        assertEquals(0, scoreYatzy(YatziCategory.ONES, new int[] {1,2,3,4,5,6}));
+    }
+
+
+
+    public int scoreYatzy(YatziCategory cat, int[] dice) {
+        return 0;
     }
 
 
